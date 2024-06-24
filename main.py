d695bb81c3728f5c508bc290d8bebcbd08ecffa6
ichimdaniel324 <57685367+ichimdaniel324@users.noreply.github.com>
2024-06-24 12:51:49 +0300

    count characters function

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     words = count_words(text)
-    print (words)
-
+    number_of_words = f"the book has {words} words"
+    character_count = count_characters(text)
+    print(character_count)
+    
 
 def get_book_text(path):
     with open(path) as f:
@@ -19,7 +21,13 @@ def count_words(string):
 
 def count_characters(string):
     lowered_string =string.lower()
-     
+    char_count = {}
+    for char in lowered_string:
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] +=1
+            else:
+                char_count[char] =1
+    return char_count
 
 main()
-# main()
