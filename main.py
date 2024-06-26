fbfa8ac85c7e66e2d788186eabb8a3231ec48dc2
ichimdaniel324 <57685367+ichimdaniel324@users.noreply.github.com>
2024-06-26 16:51:57 +0300

    finished

--- main.py
+++ main.py
@@ -4,7 +4,21 @@ def main():
     words = count_words(text)
     number_of_words = f"the book has {words} words"
     character_count = count_characters(text)
-    print(character_count)
+    
+    char_list = []
+    for char, count in character_count.items():
+        char_list.append({"char": char, "count": count})
+    
+    def sort_on(dict):
+        return dict["count"]
+
+    char_list.sort(reverse=True, key=sort_on)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{words} words found in the document")
+    for item in char_list:
+        print(f"The '{item['char']}' character was found {item['count']} times")
+    print("--- End report ---")
     
 
 def get_book_text(path):
@@ -13,21 +27,21 @@ def get_book_text(path):
 
 
 def count_words(string):
-    words =string.split()
+    words = string.split()
     counter = 0
     for word in words:
         counter += 1
     return counter
 
 def count_characters(string):
-    lowered_string =string.lower()
+    lowered_string = string.lower()
     char_count = {}
     for char in lowered_string:
-        if char.isalpha():
+        if char.isalpha():  # Check if the character is an alphabet character
             if char in char_count:
-                char_count[char] +=1
+                char_count[char] += 1
             else:
-                char_count[char] =1
+                char_count[char] = 1
     return char_count
 
 main()
